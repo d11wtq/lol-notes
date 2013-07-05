164e3bfb38b795e98f6ab038829529f17e329853
d11wtq <chris@w3style.co.uk>
2013-07-05 22:58:35 +1000

    Refactor of time-interval, using a macro defining macro

--- examples/timers.lisp
+++ examples/timers.lisp
@@ -1,14 +1,20 @@
 ;;; timers.lisp --- Timer-related stuff, like sleep.
 
+(defmacro! definterval (name base-n &rest subunits)
+  "Macro to define an interval macro."
+  `(defmacro ,(symb name '-interval) (,g!n ,g!unit)
+     `(* ,(case ,g!unit
+            (,base-n 1)
+            ,@(group subunits 2))
+         ,,g!n)))
+
 (defmacro sleep-for (n unit)
   "Sleep for a period of time given by N and UNIT"
   `(sleep (time-interval ,n ,unit)))
 
-(defmacro time-interval (n unit)
-  "Expands to a number of seconds for N and UNIT"
-  `(* ,(case unit
-         (s 1)
-         (m 60)
-         (h 3600)
-         (d 86400))
-      ,n))
+;;; -- Use cases
+
+(definterval time s
+  m 60
+  h 3600
+  d 86400)
