d292c47d9134d0ec086451c4083f385498fcead1
d11wtq <chris@w3style.co.uk>
2013-07-07 23:39:54 +1000

    Code formatting in nlet.

--- nlet.lisp
+++ nlet.lisp
@@ -1,19 +1,18 @@
 ;;; nlet.lisp --- A named, recursive let.
 
-;; This named let is tail call optimized, since it uses
-;; gotos instead of actual recursion on a stack.
-
 (defmacro! nlet (name letargs &rest body)
+  "Defines a named let, like a function with default args."
   (let ((gs (mapcar (lambda (s) (gensym)) letargs)))
     `(macrolet ((,name ,gs
        `(progn
-          (psetq ,@(apply #'nconc
-                          (mapcar #'list
-                                  ',(mapcar #'car letargs)
-                                  (list ,@gs))))
+          (psetq
+           ,@(apply #'nconc
+                    (mapcar #'list
+                            ',(mapcar #'car letargs)
+                            (list ,@gs))))
           (go ,',g!n))))
        (block ,g!b
          (let ,letargs
            (tagbody
-            ,g!n (return-from
-                     ,g!b (progn ,@body))))))))
+            ,g!n (return-from ,g!b
+                   (progn ,@body))))))))
